2f84966a5d0cc7f7d1fd4f5b0cff6431c2d62a74
Geoff Denning <gdenning@gmail.com>
2011-08-07 12:32:32 -0700

    Lesson 1: Hello World - basic edit text with button that displays a Toast.

--- android/tutorial/src/com/elasticpath/tutorial/AndroidTutorialActivity.java
+++ android/tutorial/src/com/elasticpath/tutorial/AndroidTutorialActivity.java
@@ -2,12 +2,33 @@ package com.elasticpath.tutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AndroidTutorialActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private TextView helloTextView;
+	private EditText nameEditText;
+	private Button lovesEPButton;
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		helloTextView = (TextView) findViewById(R.id.helloText);
+		nameEditText = (EditText) findViewById(R.id.nameEdit);
+		lovesEPButton = (Button) findViewById(R.id.lovesEPButton);
+
+		helloTextView.setText("Hello mobile developers!");
+		lovesEPButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				final Toast toast = Toast.makeText(AndroidTutorialActivity.this, nameEditText.getText() + " loves Elastic Path!", Toast.LENGTH_LONG);
+				toast.show();
+			}
+		});
+	}
 }
