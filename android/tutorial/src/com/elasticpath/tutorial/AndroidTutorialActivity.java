9f3f0b87387ad4efb6648a2311145456d54b5461
Geoff Denning <gdenning@gmail.com>
2011-08-07 11:46:44 -0700

    Empty Android project.

---
+++ android/tutorial/src/com/elasticpath/tutorial/AndroidTutorialActivity.java
@@ -0,0 +1,13 @@
+package com.elasticpath.tutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidTutorialActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
